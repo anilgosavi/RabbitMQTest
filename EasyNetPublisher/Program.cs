4900ab3dd490a4ee74f050a1d3d0d2c491c44d4c
agosavi <agosavi@Win10>
2018-12-02 15:29:20 +0000

    minor change

--- EasyNetPublisher/Program.cs
+++ EasyNetPublisher/Program.cs
@@ -13,7 +13,7 @@ namespace EasyNetPublisher
                 Console.WriteLine("Enter a message. 'Quit' to quit");
                 while ((input=Console.ReadLine())!="Quit")
                     {
-                    bus.Publish(new TextMessage{Text=input});
+                        bus.Publish(new TextMessage{Text=input});
                     }
             }
         }
